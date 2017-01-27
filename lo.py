91a4119415598e7ec5fe63ffc45a012b1531a213
Joe Baltimore <joebalt@skypeak.com>
2017-01-27 20:11:24 +0000

    Initial commit.

---
+++ lo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+import sys
+
+
+def main(argv):
+    print('Begin run...')
+
+    p = float(raw_input("Stock price                  : "))
+    ns = float(raw_input("Number of shares             : "))
+    c = float(raw_input("buy/sell combined commission : "))
+    if ns <= 0:
+        print("Number of shares cannot be zero, exiting...")
+        sys.exit(1)
+    dp = float(raw_input("Desired profit               : "))
+    # ct = float(raw_input("Capital gains tax (%)        : "))
+
+    pp = c / ns
+
+    print("Break even price: {:6.2f}".format(p + pp))
+
+    if dp > 0:
+        pp = pp + (dp / ns)
+        print("Desired profit sell price {:6.2f}".format(p + pp))
+
+    print('End run.')
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
